37ee4873af3d58a071c41bdb4bd39398ee97f6c1
user <user@userui-MacBookPro.local>
2025-02-22 00:47:32 +0900

    [remove] 불필요한 클래스 제거

--- src/sum.ts
+++
@@ -1,4 +0,0 @@
-// 테스트용 
-export function sum(a: number, b: number): number {
-    return a + b;
-}
--- tests/units/sum.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from '../../src/sum';
-
-test('1 + 2 should equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
-test('5 + 7 should equal 12', () => {
-    expect(sum(5, 7)).toBe(12);
-});
